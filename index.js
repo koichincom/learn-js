27e2bbc5b2429c4a5c1a3af69fa17fde0a32c097
koichi <koichi.nakayamada@gmail.com>
2024-09-27 12:52:03 -0700

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+let message = "Hello, World!";
+console.log (message);
+
+const hello = "Hello!";
+console.log (hello);
+
+let array = ["one", "two", "three"];
+console.log (array[0]);
+
+let index = 0;
+while(index < array.length){
+    
+    index++;
+}
